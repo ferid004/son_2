47b449c86d7fc3327d7aea5ab5413efc0df89094
ferid aqa <fer2004id@gmail.com>
2024-01-18 15:34:59 +0400

    get homda scss bittdi

--- client/src/components/product/index.jsx
+++ client/src/components/product/index.jsx
@@ -15,6 +15,23 @@ function Product() {
 
     return (
         <div id='product'>  
+        <div className="uptext">
+            <div className="icon">
+            <i class="fa-solid fa-bowl-food"></i>
+            </div>
+            <div className="outtext">
+                <div className="line"></div>
+                <h3>Our Menu</h3>
+                <div className="line"></div>
+            </div>
+        </div>
+        <div className="downtext">
+            <p>Breackfast</p>
+            <p>Brunch</p>
+            <p>Lunch</p>
+            <p>Dinner</p>
+        </div>
+        <div className="pp">
             {product && product.map((item) => (
                 <ul className="probox" key={item._id}>
                     <div className="up">
@@ -26,6 +43,7 @@ function Product() {
                     </div>
                 </ul>
             ))}
+        </div>
         </div>
     )
 }
